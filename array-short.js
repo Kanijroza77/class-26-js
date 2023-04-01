dfe7f907a6090208ee8b13f7a57ccc49ada27973
kanij Roza <kanijroza@gmail.com>
2023-04-01 22:24:56 +0600

    first commit

---
+++ array-short.js
@@ -0,0 +1,15 @@
+const number=[1,7,8,4,5,2,3,6,9];
+
+const numberShortList =number.sort();
+
+// console.log( numberShortList );
+
+
+
+const friends =['banana,mango ,apple,orange']
+const friendsSort=friends.sort();
+console.log(friendsSort);
+
+ 
+
+
